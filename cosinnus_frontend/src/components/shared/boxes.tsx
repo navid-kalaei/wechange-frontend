1ee549898822ac5bb6c0d99904b4465e99ed7237
Simon Liedtke <simon.liedtke@gmail.com>
2022-06-22 12:30:39 +0200

    Remove frontend files

--- cosinnus_frontend/src/components/shared/boxes.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { Box, useStyleConfig } from '@chakra-ui/react'
-
-export function StyledBox(props: any) {
-    const { variant, ...rest } = props
-    const styles = useStyleConfig('Box', { variant })
-    return <Box __css={styles} {...rest} />
-}
--- cosinnus_frontend/src/utils/auth-header.ts
+++
@@ -1,7 +0,0 @@
-export default function authHeader(accessToken: string) {
-  if (accessToken) {
-    return { Authorization: 'Bearer ' + accessToken};
-  } else {
-    return {};
-  }
-}

d300a35aa1559b1b0ebbdabbcb46538ea80dac4b
Navid Kalaei <navidkalaei@gmail.com>
2022-07-01 08:49:08 +0430

    feat: add type for NextJS layout

--- next.d.ts
+++ next.d.ts
@@ -1,22 +1,23 @@
 import type {
   NextComponentType,
-  NextPageContext,
   NextLayoutComponentType,
-} from 'next';
-import type { AppProps } from 'next/app';
+  NextPageContext,
+} from 'next'
+import type { AppProps } from 'next/app'
+
 
 declare module 'next' {
   type NextLayoutComponentType<P = {}> = NextComponentType<
-    NextPageContext,
-    any,
-    P
-    > & {
+  NextPageContext,
+  any,
+  P
+  > & {
     getLayout?: (page: ReactNode) => ReactNode;
-  };
+  }
 }
 
 declare module 'next/app' {
   type AppLayoutProps<P = {}> = AppProps & {
     Component: NextLayoutComponentType;
-  };
+  }
 }

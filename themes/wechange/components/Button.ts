7fe3990195f84375e00d2ebbb949bf344aec55e0
Navid Kalaei <navidkalaei@gmail.com>
2022-07-10 22:26:05 +0430

    feat: add brand styles for button

--- themes/wechange/components/Button.ts
+++ themes/wechange/components/Button.ts
@@ -13,13 +13,45 @@ const Button: ComponentStyleConfig = {
       h: 6,
       w: 'full',
     },
-    solid: (props: StyleFunctionProps) => ({
+    'brand-solid': (props: StyleFunctionProps) => ({
       bg: mode('teal.600', 'teal.300')(props),
       color: mode('white', 'gray.900')(props),
+      _hover: {
+        bg: mode('teal.900', 'teal.600')(props),
+      },
+      _focus: {
+        // the white ring
+        _after: {
+          content: '""',
+          position: 'absolute',
+          top: '2px',
+          bottom: '2px',
+          left: '2px',
+          right: '2px',
+          borderWidth: '2px',
+          borderColor: 'white',
+          borderRadius: 'inherit',
+          bg: 'none',
+        },
+
+        borderColor: 'teal.500',
+        bg: mode('teal.600', 'teal.300')(props),
+      },
+      _active: {
+        bg: mode('teal.800', 'teal.600')(props),
+      },
+      _disabled: {
+        color: 'white',
+        bg: 'gray.400',
+      },
     }),
-    link: (props: StyleFunctionProps) => ({
+    'brand-link': (props: StyleFunctionProps) => ({
       color: mode('teal.600', 'teal.300')(props),
       textDecoration: 'underline',
+    }),
+    'brand-loading': (props: StyleFunctionProps) => ({
+      color: mode('white', 'gray.900')(props),
+      bg: mode('teal.500', 'teal.200')(props),
     }),
   },
 }

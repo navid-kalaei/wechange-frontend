fdfad06e7011c62ea9d39bf9092b0aeaaffe5fcd
Navid Kalaei <navidkalaei@gmail.com>
2022-07-18 06:43:53 +0430

    feat: add  Alert and CloseButton styles to the default theme

--- themes/default/components/index.ts
+++ themes/default/components/index.ts
@@ -1,12 +1,16 @@
 import { ThemeComponents } from '@chakra-ui/theme/dist/declarations/src/theme.types'
 // eslint-disable-next-line sort-imports
+import Alert from './Alert'
 import Card from './Card'
+import CloseButton from './CloseButton'
 import FormHelperText from './FormHelperText'
 import Link from './Link'
 
 
 const components: ThemeComponents = {
+  Alert,
   Card,
+  CloseButton,
   FormHelperText,
   Link,
 }

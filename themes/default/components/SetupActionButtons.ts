04565e36012551cec7cdb8e0d292b7b38f9fcf33
Navid Kalaei <navidkalaei@gmail.com>
2022-07-25 07:19:10 +0430

    fix: reduce spacing for buttons by 2 because it has a default margin-inline-start of 2 which cannot be overwritten by any means

--- themes/default/components/SetupActionButtons.ts
+++ themes/default/components/SetupActionButtons.ts
@@ -3,7 +3,7 @@ import type { ComponentStyleConfig } from '@chakra-ui/theme'
 
 const SetupActionButtons: ComponentStyleConfig = {
   baseStyle: {
-    gap: 4,
+    gap: 2,
     justifyContent: 'end',
     w: 'full',
   },
